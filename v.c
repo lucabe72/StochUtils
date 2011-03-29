f8d3cd16bd82521318c933446a7231697717b6d8
Luca Abeni <luca.abeni@unitn.it>
2011-03-29 09:31:02 +0200

    Another small optimization

--- v.c
+++ v.c
@@ -12,12 +12,15 @@ struct pmf *v_compute(const struct pmf *w, const struct pmf *c, unsigned int max
   for (vi = 0; vi < max; vi += step) {
     double p;
     unsigned int cmax;
+    unsigned int min;
 
     p = 0;
     cmax = pmf_max(c);
+    min = cmax < vi ? cmax : vi;
 //    if (cmax > vi) {
 //      cmax = vi;
 //    }
+#if 0
     for (i = 0; i <= cmax; i++) {
       if (i <= vi) {
         p += pmf_get(c, i) * (1.0 - pmf_get(w, (int)vi - (int)i));
@@ -25,6 +28,14 @@ struct pmf *v_compute(const struct pmf *w, const struct pmf *c, unsigned int max
         p += pmf_get(c, i);
       }
     }
+#else
+    for (i = 0; i <= min; i++) {
+      p += pmf_get(c, i) * (1.0 - pmf_get(w, (int)vi - (int)i));
+    }
+    for (i = vi + 1; i <= cmax; i++) {
+      p += pmf_get(c, i);
+    }
+#endif
     p += pmf_tail(c);
     pmf_set(v, vi, 1.0 - p);
   }
