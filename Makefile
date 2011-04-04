d920f3bd05e075d5e30fd0b8426fc65854fe560e
Luca Abeni <luca.abeni@unitn.it>
2011-04-04 21:12:34 +0200

    Random PMF generation...

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS = -Wall -Wextra -g -O8
 LDLIBS = -lm
 
-all: meschac avg pmftest pmf-y pmf-yt pmf-rnd matrix
+all: meschac avg pmftest pmf-y pmf-yt pmf-rnd matrix pmfgen
 
 avg: avg.o pmf.o pmf-file.o gamma.o y.o
 avg-c1: avg-c1.o pmf.o pmf-file.o
@@ -10,6 +10,8 @@ pmf-y: pmf-y.o pmf.o pmf-file.o gamma.o y.o v.o pmf-modify.o dl.o
 pmf-yt: pmf-yt.o pmf.o pmf-file.o gamma.o z.o y.o v.o pmf-modify.o dl.o
 pmf-rnd: pmf-rnd.o pmf.o pmf-file.o pmf-sample.o
 matrix: matrix.o pmf.o pmf-file.o cdf.o qbdm.o meschac/meschach.a
+pmfgen: pmfgen.o pmf.o
+
 meschac/meschach.a:
 	make -C meschac
 clean:
---
+++ pmfgen.c
@@ -0,0 +1,76 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "pmf.h"
+
+static int offs = 100;
+static int size = 500;
+static int samples = 10000;
+
+static void pmf_rnd(struct pmf *p, int offs, int size, int samples)
+{
+  int i;
+  double sum = 0;
+
+  for (i = offs; i < offs + size; i++) {
+    unsigned long long int val;
+
+    val = rand();
+    val = (val* samples) / RAND_MAX;
+
+    pmf_set(p, i, val);
+    sum += val;
+  }
+
+  for (i = offs; i < offs + size; i++) {
+    pmf_set(p, i, pmf_get(p, i) / sum);
+  }
+}
+
+static int opts_parse(int argc, char *argv[])
+{
+  int opt;
+
+  while ((opt = getopt(argc, argv, "o:s:S:")) != -1) {
+    switch (opt) {
+      case 'o':
+        offs = atoi(optarg);
+        break;
+      case 's':
+        size = atoi(optarg);
+        break;
+      case 'S':
+        samples = atoi(optarg);
+        break;
+      default: /* ’?’ */
+        fprintf(stderr, "Wrong parameter %c\n", opt);
+        exit(EXIT_FAILURE);
+     }
+  }
+
+  return optind;
+}
+
+
+int main(int argc, char *argv[])
+{
+  struct pmf *p;
+  int i;
+
+  opts_parse(argc, argv);
+  p = pmf_create(size + offs, 0);
+  pmf_rnd(p, offs, size, samples);
+
+  if (pmf_check(p)) {
+    printf("Bad PMF: %d | %f!!!\n", pmf_check(p), pmf_sum(p, 0));
+
+    return -1;
+  }
+  for (i = 0; i < pmf_max(p); i++) {
+    printf("P{x = %d} = %f\n", i, pmf_get(p, i));
+  }
+  printf("P{x >= %d} = %f\n", pmf_max(p), pmf_tail(p));
+
+  return 0;
+}
