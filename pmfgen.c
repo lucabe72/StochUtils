c888585710a7ba42936be36c1265e139d08bdb35
Nicola Manica <nicola.manica@disi.unitn.it>
2011-05-05 18:20:47 +0200

    Add a beta function generator

--- pmfgen.c
+++ pmfgen.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include "pmf.h"
 
@@ -9,6 +10,38 @@ static int size = 500;
 static int samples = 10000;
 static int verbose;
 static int seed;
+static int beta=0;
+static double a=2;
+static double b=2;
+
+static double beta_n(double a, double b)
+{
+  double sumgamma,agamma,bgamma;
+  double val;
+  agamma=tgamma(a);
+  bgamma=tgamma(b);
+  sumgamma=tgamma(a+b);
+  val = rand();
+  val = (val) / RAND_MAX;
+  val = pow(val,a)*pow(1-val,b-1);
+  val = ((double)sumgamma/(double)(agamma*bgamma))*val;
+  return val;
+}
+
+
+static void pmf_beta(struct pmf *p, double a, double b, int offs, int size){
+  int i;
+  double sum = 0;
+  for (i = offs; i < offs + size; i++) {
+    double val;
+    val=beta_n(a,b);
+    pmf_set(p, i, val);
+    sum += val;
+  }
+  for (i = offs; i < offs + size; i++) {
+    pmf_set(p, i, pmf_get(p, i) / sum);
+  }
+}
 
 static void pmf_rnd(struct pmf *p, int offs, int size, int samples)
 {
@@ -20,7 +53,6 @@ static void pmf_rnd(struct pmf *p, int offs, int size, int samples)
 
     val = rand();
     val = (val* samples) / RAND_MAX;
-
     pmf_set(p, i, val);
     sum += val;
   }
@@ -34,8 +66,11 @@ static int opts_parse(int argc, char *argv[])
 {
   int opt;
 
-  while ((opt = getopt(argc, argv, "o:s:S:r:v")) != -1) {
+  while ((opt = getopt(argc, argv, "Bo:s:S:r:a:b:v")) != -1) {
     switch (opt) {
+      case 'B':
+        beta=1;
+	break;
       case 'o':
         offs = atoi(optarg);
         break;
@@ -48,6 +83,12 @@ static int opts_parse(int argc, char *argv[])
       case 'r':
         seed = atoi(optarg);
         break;
+      case 'a':
+        a = atof(optarg);
+	break;
+      case 'b':
+        b = atof(optarg);
+	break;
       case 'v':
         verbose = 1;
         break;
@@ -71,7 +112,10 @@ int main(int argc, char *argv[])
     srand(seed);
   }
   p = pmf_create(size + offs, 0);
-  pmf_rnd(p, offs, size, samples);
+  //pmf_rnd(p, offs, size, samples);
+  if (beta)
+    pmf_beta(p, a, b, offs, size);
+  else pmf_rnd(p, offs, size, samples);
 
   if (pmf_check(p)) {
     printf("Bad PMF: %d | %f!!!\n", pmf_check(p), pmf_sum(p, 0));
