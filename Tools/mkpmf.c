59b6346aaaec32530aab0c69185e968b986e207f
Luca Abeni <luca.abeni@unitn.it>
2012-01-25 16:27:34 +0100

    Add utility to transform a sequence of samples in a PMF

---
+++ Tools/mkpmf.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+struct list_elem {
+    int value;
+    int cnt;
+    struct list_elem *next;
+};
+
+struct list_elem *list_insert(struct list_elem *l, int value)
+{
+    struct list_elem *p, *p1, *res;
+
+    p1 = NULL;
+    p = l;
+    while ((p != NULL) && (p->value <= value)) {
+        if (p->value == value) {
+            p->cnt++;
+
+            return l;
+        }
+        p1 = p;
+        p = p->next;
+    }
+
+    res = malloc(sizeof(struct list_elem));
+    if (res == NULL) {
+        return NULL;
+    }
+    res->value = value;
+    res->cnt = 1;
+    res->next = p;
+
+    if (p1 == NULL) {
+        return res;
+    }
+    p1->next = res;
+
+    return l;
+}
+
+void cdf_print(struct list_elem *l)
+{
+    int cnt, total;
+    struct list_elem *p;
+
+    total = 0;
+    p = l;
+    while (p != NULL) {
+        total += p->cnt;
+	p = p->next;
+    }
+
+    cnt = 0;
+    p = l;
+    while (p != NULL) {
+        cnt += p->cnt;
+        printf("%d %d %16.14f\n", p->value, p->cnt, (double)p->cnt / (double)total);
+	p = p->next;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int val;
+    struct list_elem *h;
+    int res;
+
+    h = NULL;
+    while(!feof(stdin)) {
+        res = scanf("%d\n", &val);
+        if (res == 1) {
+          h = list_insert(h, val);
+        }
+    }
+
+    cdf_print(h);
+    return 0;
+}
